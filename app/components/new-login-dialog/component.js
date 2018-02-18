d261331e8b9054f324ce58eeed8815947010ff12
Tim Evans <tim.c.evans@me.com>
2018-02-18 13:45:43 -0500

    add ability to add new logins

--- app/components/new-login-dialog/component.js
+++ app/components/new-login-dialog/component.js
@@ -1,8 +1,13 @@
 import Component from '@ember/component';
+import { get } from '@ember/object';
+import { not } from '@ember/object/computed';
 
 export default Component.extend({
+  isFormOpen: not('logins.length'),
   actions: {
-    submit() {
+    submit(_, changes) {
+      changes.role = 'staff';
+      return get(this, 'onsubmit')(changes).then(get(this, 'dismiss'));
     }
   }
 });
--- app/routes/person.js
+++ app/routes/person.js
@@ -2,8 +2,10 @@ import Resource from './resource';
 
 export default Resource.extend({
   actions: {
-    addLogin(params) {
-
+    addLogin(model, attributes) {
+      attributes.person = model;
+      let login = this.store.createRecord('authorization', attributes);
+      return login.save();
     }
   }
 });
