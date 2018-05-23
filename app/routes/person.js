9f62d057aa1e6a44be130413959b73e8c387c9be
Tim Evans <tim.c.evans@me.com>
2018-05-22 22:36:56 -0400

    fix creating logins for people

--- app/routes/person.js
+++ app/routes/person.js
@@ -4,8 +4,15 @@ export default Resource.extend({
   actions: {
     addLogin(model, attributes) {
       attributes.person = model;
+      attributes.role = 'staff';
       let login = this.store.createRecord('authorization', attributes);
       return login.save();
+    },
+    deleteLogin(model) {
+      model.deleteRecord();
+      return model.save().then(() => {
+        this.currentModel.authorizations.removeObject(model);
+      });
     }
   }
 });
