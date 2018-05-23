224364ec6fa42b624e1abe64da35ae279977c720
Tim Evans <tim.c.evans@me.com>
2018-05-22 22:44:01 -0400

    fix permissioning for people when they recieve logins

--- app/routes/person.js
+++ app/routes/person.js
@@ -1,16 +1,24 @@
 import Resource from './resource';
+import RSVP from 'rsvp';
 
 export default Resource.extend({
   actions: {
     addLogin(model, attributes) {
       attributes.person = model;
-      attributes.role = 'staff';
+
+      // Adding logins automatically gives access to the admin portal
+      if (model.role !== 'staff') {
+        model.set('role', 'staff');
+      }
       let login = this.store.createRecord('authorization', attributes);
-      return login.save();
+      return RSVP.all([model.save(), login.save()]);
     },
     deleteLogin(model) {
+      if (this.currentModel.authorizations.length === 1) {
+        this.currentModel.set('role', 'member');
+      }
       model.deleteRecord();
-      return model.save().then(() => {
+      return RSVP.all([model.save(), this.currentModel.save()]).then(() => {
         this.currentModel.authorizations.removeObject(model);
       });
     }
