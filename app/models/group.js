7018724e73426831e08b8a2fb0ade620326da6ae
Tim Evans <tim.c.evans@me.com>
2018-05-27 19:27:04 -0400

    add view live button to posts

--- app/models/group.js
+++ app/models/group.js
@@ -1,4 +1,5 @@
 import DS from 'ember-data';
+import { computed } from '@ember/object';
 import Model from './application';
 
 const { attr, belongsTo, hasMany } = DS;
@@ -13,5 +14,13 @@ export default Model.extend({
   timezone: attr('string'),
   locale: attr('string'),
   stripePublishableKey: attr('string'),
-  stripeSecretKey: attr('string')
+  stripeSecretKey: attr('string'),
+
+  url: computed('hostname', function() {
+    if (this.hostname.indexOf('localhost') === 0) {
+      return `http://${this.hostname}`;
+    } else {
+      return `https://${this.hostname}`;
+    }
+  })
 });
