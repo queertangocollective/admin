96dd17f373a8d9906c66e1d9f1ac6bd693b44daf
Tim Evans <tim.c.evans@me.com>
2018-08-04 11:15:31 -0400

    fix sorting bug

--- app/computed/sort.js
+++ app/computed/sort.js
@@ -4,6 +4,9 @@ import { camelize } from '@ember/string';
 export default function (collectionPath, sortPath) {
   return computed(collectionPath + '.[]', collectionPath + '.isLoaded', sortPath, function () {
     let sort = this.get(sortPath);
+    if (sort == null) {
+      return this.get(collectionPath);
+    }
     if (sort.indexOf('-') === 0) {
       return this.get(collectionPath).sortBy(camelize(sort.slice(1)) + ':desc');
     }
